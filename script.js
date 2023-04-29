a00a1cdd59ee429321efabf52ed9617c7f7b33c6
Ubuntu <metalsyntax@penguin.lxd>
2023-04-29 02:26:06 -0400

    Added responsive design features to improve display on smaller screens

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dolarToday = document.getElementById("dolar-bcv");
 const monitorDolarVzla = document.getElementById("monitor-dolar-vzla");
+const dataVariation = document.querySelector(".data-variation");
+const dataDiferential = document.querySelector(".data-diferential");
 
 fetch("https://venecodollar.vercel.app/api/v1/dollar")
   .then(response => response.json())
@@ -10,7 +12,8 @@ fetch("https://venecodollar.vercel.app/api/v1/dollar")
         const dollar = entity.info.dollar;
         const updatedDate = entity.info.updatedDate;
         const variation = (data.Data.entities[1].info.dollar/data.Data.entities[0].info.dollar-1)*100;
-      
+        const diferential = data.Data.entities[1].info.dollar/data.Data.entities[0].info.dollar
+
       if (title === "BCV (Oficial)") {
         dolarToday.querySelector(".data-title").textContent = title;
         dolarToday.querySelector(".data-value").textContent = `${dollar} VES`;
@@ -19,7 +22,15 @@ fetch("https://venecodollar.vercel.app/api/v1/dollar")
         monitorDolarVzla.querySelector(".data-title").textContent = title;
         monitorDolarVzla.querySelector(".data-value").textContent = `${dollar} VES`;
         monitorDolarVzla.querySelector(".updated-date").textContent = updatedDate;
-        monitorDolarVzla.querySelector(".data-variation").textContent = `${Number(variation.toFixed(2))}%`;
+      }
+
+      dataVariation.textContent = `${Number(variation.toFixed(2))}%`;
+      dataDiferential.textContent = Number(diferential.toFixed(2));
+
+      if (variation > 0) {
+        dataVariation.style.color = "green";
+      } else {
+        dataVariation.style.color = "red";
       }
     });
 })
