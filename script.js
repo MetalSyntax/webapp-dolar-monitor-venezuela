32641d402c4c9e5507e229a36c3ab52530b0c422
Wonder Diaz <wonderdxk@gmail.com>
2024-02-27 22:45:41 -0400

    fix conditional and change title

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const dolarToday = document.getElementById("dolar-bcv");
+const dolarBCV = document.getElementById("dolar-bcv");
 const monitorDolarVzla = document.getElementById("monitor-dolar-vzla");
 const dataVariation = document.querySelector(".data-variation");
 const dataDiferential = document.querySelector(".data-diferential");
@@ -10,12 +10,12 @@ table.style.display = "none";
 section.style.display = "none";
 spinner.style.display = "block";
 
-
 fetch("https://venecodollar.vercel.app/api/v1/dollar")
   .then(response => response.json())
   .then(data => {
     const entities = data.Data.entities;
     entities.forEach(entity => {
+      console.log(entity)
       const title = entity.info.title;
       const dollar = entity.info.dollar;
       const updatedDate = entity.info.updatedDate;
@@ -25,12 +25,12 @@ fetch("https://venecodollar.vercel.app/api/v1/dollar")
       table.style.display = "table"
       section.style.display = "block"
       let imageSrc = "";
-      if (title === "BCV (Oficial)") {
+      if (title === "Dólar BCV") {
         imageSrc = "images/dolar-bcv.png";
-        dolarToday.querySelector(".data-title").innerHTML = `<img class="img-title" src="${imageSrc}" alt="Banco Central de Venezuela Logo" style="width: 20px; margin-right: 5px;">${title}`;
-        dolarToday.querySelector(".data-value").textContent = `${dollar} VES`;
-        dolarToday.querySelector(".updated-date").textContent = updatedDate;
-      } else if (title === "@EnParaleloVzla3") {
+        dolarBCV.querySelector(".data-title").innerHTML = `<img class="img-title" src="${imageSrc}" alt="Banco Central de Venezuela Logo" style="width: 20px; margin-right: 5px;">${title}`;
+        dolarBCV.querySelector(".data-value").textContent = `${dollar} VES`;
+        dolarBCV.querySelector(".updated-date").textContent = updatedDate;
+      } else if (title === "Dólar Monitor") {
         imageSrc = "images/dolar-monitor.jpeg";
         monitorDolarVzla.querySelector(".data-title").innerHTML = `<img class="img-title" src="${imageSrc}" alt="Monitor Dolar Vzla Logo" style="width: 20px; margin-right: 5px;">${title}`;
         monitorDolarVzla.querySelector(".data-value").textContent = `${dollar} VES`;
